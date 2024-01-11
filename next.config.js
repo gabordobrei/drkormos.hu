99b8401f59cde5841e6516685e0a6636b0d5e534
Gábor Döbrei <gabor@dobrei.com>
2024-01-11 15:51:05 +0100

    Allow staging origin for server actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['drkormos-hu.pages.dev']
+    }
+  }
+}
 
 module.exports = nextConfig
