45fde3a706ad722297f7c29f95f22d004fd77a80
Gábor Döbrei <gabor@dobrei.com>
2024-01-15 13:17:16 +0100

    Remove redundant design element

--- src/app/kapcsolat/page.tsx
+++ src/app/kapcsolat/page.tsx
@@ -30,18 +30,6 @@ function Map() {
       <div className="mx-auto grid max-w-7xl grid-cols-1 py-20 lg:grid-cols-2">
         <div className="relative px-6 sm:pt-32 lg:static lg:px-8">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-            <div
-              className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-              aria-hidden="true"
-            >
-              <div
-                className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-                style={{
-                  clipPath:
-                    'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
-                }}
-              />
-            </div>
 
             <h2 className="text-3xl font-bold tracking-tight text-gray-900">Elérhetőségek</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
