058dd4f5417333e39b55fdca539e62ea43377632
Gábor Döbrei <gabor@dobrei.com>
2024-06-03 15:16:49 +0200

    Add new profile page

--- src/app/rolam/page.tsx
+++ src/app/rolam/page.tsx
@@ -3,7 +3,7 @@ import Footer from '@/app/(components)/Footer';
 import Image from 'next/image';
 import { testimonials } from '@/Content';
 import { ReactElement } from 'react';
-import profileSquare from '@/../public/profile-square.jpg';
+import profileLarge from '@/../public/profile-large.jpg';
 
 export const runtime = 'edge';
 
@@ -57,9 +57,9 @@ function Hero() {
             </p>
           </div>
           <Image
-            src={profileSquare}
+            src={profileLarge}
             alt="Dr. Kormos Zsuzsanna"
-            className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover brightness-125 sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+            className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
           />
         </div>
       </div>
